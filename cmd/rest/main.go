f4fb584f843871349e408526a6c560e3008e5740
Diamond Phanes <zakhar.griaznov@gmail.com>
2024-08-15 12:14:39 +0300

    added func to load data from data.csv | added col ID | added func to delete indices

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,29 +1,23 @@
 package main
 
-import "github.com/zkhrg/go_day03/internal/elasticsearch"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/zkhrg/go_day03/internal/elasticsearch"
+)
 
 func main() {
-	mapping := `{
-	"settings": {
-		"number_of_shards": 1
-	},
-	"mappings": {
-		"properties": {
-			"name": {
-				"type":  "text"
-			},
-			"address": {
-				"type":  "text"
-			},
-			"phone": {
-				"type":  "text"
-			},
-			"location": {
-				"type": "geo_point"
-			}
-		}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading .env file: %s", err)
 	}
-}`
+	modulePath := os.Getenv("MODULE_PATH")
+	mapping, err := os.ReadFile(modulePath + "internal/elasticsearch/schemas/places_mapping.json")
+	if err != nil {
+		log.Fatalf("Error reading mapping file: %s", err)
+	}
 	elasticsearch.InitClient()
-	elasticsearch.CreateIndex("loops", mapping)
+	elasticsearch.CreateIndex("places", string(mapping))
+	elasticsearch.Indexing("places", modulePath+"datasets/data.csv")
 }
--- internal/elasticsearch/client.go
+++ internal/elasticsearch/client.go
@@ -1,16 +1,21 @@
 package elasticsearch
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/joho/godotenv"
 )
 
 var es *elasticsearch.Client
+
+const batchSize = 500
 
 func InitClient() {
 	var err error
@@ -66,4 +71,130 @@ func CreateIndex(indexName string, mapping string) error {
 	}
 
 	return nil
+}
+
+func DeleteIndex(indexName string) error {
+	res, err := es.Indices.Delete([]string{indexName})
+	if err != nil {
+		return fmt.Errorf("error deleting index: %s", err)
+	}
+	defer res.Body.Close()
+	return nil
+}
+
+func Indexing(indexName string, CSVFileName string) {
+	headerMap := map[string]string{
+		"Name":      "name",
+		"Address":   "address",
+		"Phone":     "phone",
+		"Longitude": "location.lon",
+		"Latitude":  "location.lat",
+		"ID":        "id",
+	}
+	file, err := os.Open(CSVFileName)
+	if err != nil {
+		log.Fatalf("error opening csv file: %s", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = '\t'
+
+	// Чтение заголовков
+	headers, err := reader.Read()
+	if err != nil {
+		log.Fatalf("error reading headers: %s", err)
+	}
+
+	var wg sync.WaitGroup
+
+	var batch []map[string]interface{}
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err.Error() != "EOF" {
+				log.Fatalf("error reading CSV file: %s", err)
+			}
+			break
+		}
+
+		doc := make(map[string]interface{})
+		for i, value := range record {
+			if i < len(headers) {
+				key := headerMap[headers[i]]
+				if key == "phone" && value != "" && !strings.HasPrefix(value, "+7") {
+					value = "+7" + value
+				}
+				if strings.HasPrefix(key, "location.") {
+					if doc["location"] == nil {
+						doc["location"] = make(map[string]interface{})
+					}
+					locMap := doc["location"].(map[string]interface{})
+					locKey := strings.TrimPrefix(key, "location.")
+					locMap[locKey] = value
+				} else {
+					doc[key] = value
+				}
+			}
+		}
+
+		batch = append(batch, doc)
+		if len(batch) >= batchSize {
+			wg.Add(1)
+			go func(batch []map[string]interface{}) {
+				defer wg.Done()
+				sendBatch(es, indexName, batch)
+			}(batch)
+			batch = nil
+		}
+	}
+
+	if len(batch) > 0 {
+		wg.Add(1)
+		go func(batch []map[string]interface{}) {
+			defer wg.Done()
+			sendBatch(es, indexName, batch)
+		}(batch)
+	}
+
+	wg.Wait()
+
+	fmt.Println("data indexing completed.")
+}
+
+func sendBatch(es *elasticsearch.Client, indexName string, batch []map[string]interface{}) {
+	var buf strings.Builder
+	for _, doc := range batch {
+
+		// Формируем метаданные для действия индексации с указанием идентификатора
+		meta := []byte(fmt.Sprintf(`{ "index" : { "_id" : "%s" } }`, doc["id"]))
+
+		// Преобразуем документ в JSON
+		data, _ := json.Marshal(doc)
+
+		// Добавляем метаданные и данные в буфер
+		buf.Write(meta)
+		buf.WriteByte('\n')
+		buf.Write(data)
+		buf.WriteByte('\n')
+		fmt.Println(doc)
+	}
+
+	res, err := es.Bulk(
+		strings.NewReader(buf.String()),
+		es.Bulk.WithIndex(indexName),
+		es.Bulk.WithRefresh("true"),
+	)
+	if err != nil {
+		log.Printf("error indexing batch: %s", err)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		log.Printf("[%s] error indexing batch: %s", res.Status(), res.String())
+	} else {
+		log.Printf("batch indexed successfully")
+	}
 }
