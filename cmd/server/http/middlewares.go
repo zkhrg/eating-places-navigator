446f63a75fc44568b009edba9ed24fe7931e089b
Diamond Phanes <zakhar.griaznov@gmail.com>
2024-09-02 12:46:17 +0300

    removed comments

--- cmd/server/http/middlewares.go
+++ cmd/server/http/middlewares.go
@@ -30,23 +30,19 @@ func ChainMiddleware(handler http.Handler, middlewares ...func(http.Handler) htt
 func PaginationMiddleware(a *api.API) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Получаем значение параметра `page`
 			pageParam := r.URL.Query().Get("page")
 
-			// Проверяем наличие параметра `page`
 			if pageParam == "" {
 				http.Error(w, "Missing 'page' parameter", http.StatusBadRequest)
 				return
 			}
 
-			// Преобразуем параметр `page` в int
 			page, err := strconv.Atoi(pageParam)
 			if err != nil || page < 1 || page > api.GetPagesCount(10, a.Store.GetTotalRecords()) {
 				http.Error(w, "'page' parameter must be a positive integer and dont overflow pages count", http.StatusBadRequest)
 				return
 			}
 
-			// Передаем параметр `page` в контексте
 			ctx := context.WithValue(r.Context(), PageContextKey, page)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
@@ -55,17 +51,14 @@ func PaginationMiddleware(a *api.API) func(http.Handler) http.Handler {
 
 func LatLonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Получаем значение параметров `lat` и `lon`
 		latParam := r.URL.Query().Get("lat")
 		lonParam := r.URL.Query().Get("lon")
 
-		// Проверяем наличие параметров `lat` и `lon`
 		if latParam == "" || lonParam == "" {
 			http.Error(w, "Missing 'lat' or 'lon' parameter", http.StatusBadRequest)
 			return
 		}
 
-		// Преобразуем параметры `lat` и `lon` в float64
 		lat, err := strconv.ParseFloat(latParam, 64)
 		if err != nil {
 			http.Error(w, "'lat' parameter must be a valid float", http.StatusBadRequest)
@@ -78,7 +71,6 @@ func LatLonMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
-		// Передаем параметры `lat` и `lon` в контексте
 		ctx := context.WithValue(r.Context(), LatContextKey, lat)
 		ctx = context.WithValue(ctx, LonContextKey, lon)
 
@@ -124,7 +116,6 @@ func ValidateTokenMiddleware(a *api.API) func(http.Handler) http.Handler {
 			}
 			token := parts[1]
 
-			// Проверка токена
 			tokenValid := a.ValidateToken(token)
 			if !tokenValid {
 				http.Error(w, "Invalid token", http.StatusUnauthorized)
@@ -137,16 +128,13 @@ func ValidateTokenMiddleware(a *api.API) func(http.Handler) http.Handler {
 
 func UsernameMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Получаем значение параметра `username`
 		usernameParam := r.URL.Query().Get("username")
 
-		// Проверяем наличие параметра `username`
 		if usernameParam == "" {
 			http.Error(w, "Missing 'username' parameter", http.StatusBadRequest)
 			return
 		}
 
-		// Передаем параметр `username` в контексте
 		ctx := context.WithValue(r.Context(), UsernameContextKey, usernameParam)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
